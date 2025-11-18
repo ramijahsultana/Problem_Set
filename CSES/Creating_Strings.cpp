1e24779d7096dcf26817693b707c770dee2df961
Rafa <ramijahsultana@gmail.com>
2025-11-19 01:48:43 +0600

    CSES problems

---
+++ CSES/Creating_Strings.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    vector<string> ans;
+    ans.push_back(s);
+
+    while(next_permutation(s.begin(), s.end()))
+    {
+        ans.push_back(s);
+    }
+
+    cout << ans.size() << "\n";
+
+    for(auto x : ans)
+        cout << x << "\n";
+
+    return 0;
+}
